6102d6a707769cedf6f5df8ca97c77e0c5fa74c7
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2019-12-13 05:11:21 +0530

    Added isEqualTo test cases with implementation
    It gives true if testLine and other line are equal

--- src/line.js
+++ src/line.js
@@ -1,3 +1,7 @@
+const isPointsEqual = function(point1, point2) {
+  return point1.x == point2.x && point1.y == point2.y;
+};
+
 class Line {
   constructor(endA, endB) {
     this.endA = endA;
@@ -6,5 +10,11 @@ class Line {
   toString() {
     return `Line (${this.endA.x},${this.endA.y}) to (${this.endB.x},${this.endB.y})`;
   }
+  isEqualTo(otherLine) {
+    return (
+      isPointsEqual(this.endA, otherLine.endA) &&
+      isPointsEqual(this.endB, otherLine.endB)
+    );
+  }
 }
-module.exports = Line;
+module.exports = { Line };
--- test/testLine.js
+++ test/testLine.js
@@ -1,9 +1,9 @@
 const assert = require("assert");
-const Line = require("../src/line.js");
+const { Line } = require("../src/line.js");
 
 describe("Line", function() {
   describe("tostring", function() {
-    it.only("It should give string form of the given line", function() {
+    it("It should give string form of the given line", function() {
       const endA = { x: 1, y: 2 };
       const endB = { x: 3, y: 4 };
       const testLine = new Line(endA, endB);
@@ -12,6 +12,15 @@ describe("Line", function() {
       const testLine2 = new Line(endA, endB);
       const expectedValue = testLine2.toString();
       assert.strictEqual(actualValue, expectedValue);
+    });
+  });
+  describe("isEqualTo", function() {
+    it("It should give true if two lines are equal", function() {
+      const endA = { x: 1, y: 2 };
+      const endB = { x: 3, y: 4 };
+      const testLine = new Line(endA, endB);
+      const otherLine = { endA: { x: 1, y: 2 }, endB: { x: 3, y: 4 } };
+      assert.ok(testLine.isEqualTo(otherLine));
     });
   });
 });

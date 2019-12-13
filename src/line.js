e6ea902bc07376bd0b5044483dc566d97ee83a0c
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2019-12-13 12:00:27 +0530

    Modified isEqualTo

--- src/line.js
+++ src/line.js
@@ -2,10 +2,6 @@ const arePointsEqual = function(point1, point2) {
   return point1.x === point2.x && point1.y === point2.y;
 };
 
-const isTypeSame = function(otherLine) {
-  return otherLine instanceof Line;
-};
-
 class Line {
   constructor(endA, endB) {
     this.endA = { ...endA };
@@ -15,10 +11,11 @@ class Line {
     return `Line (${this.endA.x},${this.endA.y}) to (${this.endB.x},${this.endB.y})`;
   }
   isEqualTo(otherLine) {
-    const isLineObj = isTypeSame(otherLine);
-    const areEndAsEqual = arePointsEqual(this.endA, otherLine.endA);
-    const areEndBsEqual = arePointsEqual(this.endB, otherLine.endB);
-    return isLineObj && areEndAsEqual && areEndBsEqual;
+    return (
+      otherLine instanceof Line &&
+      arePointsEqual(this.endA, otherLine.endA) &&
+      arePointsEqual(this.endB, otherLine.endB)
+    );
   }
 }
 module.exports = { Line };

f67fb80ba2fcdc074a21bf1de149c33af416ca2a
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2019-12-13 10:58:15 +0530

    Refactored testLine and line

--- src/line.js
+++ src/line.js
@@ -2,16 +2,21 @@ const arePointsEqual = function(point1, point2) {
   return point1.x === point2.x && point1.y === point2.y;
 };
 
+const isTypeSame = function(otherLine) {
+  return otherLine instanceof Line;
+};
+
 class Line {
   constructor(endA, endB) {
-    this.endA = endA;
-    this.endB = endB;
+    this.endA = { x: endA.x, y: endA.y };
+    this.endB = { x: endB.x, y: endB.y };
   }
   toString() {
     return `Line (${this.endA.x},${this.endA.y}) to (${this.endB.x},${this.endB.y})`;
   }
   isEqualTo(otherLine) {
     return (
+      isTypeSame(otherLine) &&
       arePointsEqual(this.endA, otherLine.endA) &&
       arePointsEqual(this.endB, otherLine.endB)
     );

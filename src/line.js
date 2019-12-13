ad34e9ee499d3b3d6d0341e7184e25b5eeb429f9
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2019-12-13 16:07:29 +0530

    Code Refactored

--- src/line.js
+++ src/line.js
@@ -1,20 +1,20 @@
 const arePointsEqual = function(point1, point2) {
-  return point1.x === point2.x && point1.y === point2.y;
+  return point1.x == point2.x && point1.y == point2.y;
 };
 
 class Line {
   constructor(endA, endB) {
-    this.endA = { ...endA };
-    this.endB = { ...endB };
+    this.endA = { x: endA.x, y: endA.y };
+    this.endB = { x: endB.x, y: endB.y };
   }
   toString() {
     return `Line (${this.endA.x},${this.endA.y}) to (${this.endB.x},${this.endB.y})`;
   }
-  isEqualTo(otherLine) {
+  isEqualTo(other) {
+    if (!(other instanceof Line)) return false;
     return (
-      otherLine instanceof Line &&
-      arePointsEqual(this.endA, otherLine.endA) &&
-      arePointsEqual(this.endB, otherLine.endB)
+      arePointsEqual(this.endA, other.endA) &&
+      arePointsEqual(this.endB, other.endB)
     );
   }
 }
--- test/testLine.js
+++ test/testLine.js
@@ -2,16 +2,13 @@ const assert = require("chai").assert;
 const { Line } = require("../src/line.js");
 
 describe("Line", function() {
-  describe("tostring", function() {
+  describe("toString", function() {
     it("It should give string form of the given line", function() {
       const endA = { x: 1, y: 2 };
       const endB = { x: 3, y: 4 };
       const testLine = new Line(endA, endB);
       const actualValue = testLine.toString();
       assert.strictEqual(actualValue, `Line (1,2) to (3,4)`);
-      const testLine2 = new Line(endA, endB);
-      const expectedValue = testLine2.toString();
-      assert.strictEqual(actualValue, expectedValue);
     });
   });
   describe("isEqualTo", function() {
@@ -19,15 +16,15 @@ describe("Line", function() {
       const endA = { x: 1, y: 2 };
       const endB = { x: 3, y: 4 };
       const testLine = new Line(endA, endB);
-      const otherLine = new Line(endA, endB);
-      assert.ok(testLine.isEqualTo(otherLine));
+      const other = new Line(endA, endB);
+      assert.ok(testLine.isEqualTo(other));
     });
     it("It should give false if fields of both lines are  equal but not instance of line", function() {
       const endA = { x: 1, y: 2 };
       const endB = { x: 3, y: 4 };
       const testLine = new Line(endA, endB);
-      const otherLine = { endA: { x: 1, y: 2 }, endB: { x: 3, y: 4 } };
-      const actualValue = testLine.isEqualTo(otherLine);
+      const other = { endA: { x: 1, y: 2 }, endB: { x: 3, y: 4 } };
+      const actualValue = testLine.isEqualTo(other);
       assert.isNotOk(actualValue);
     });
   });

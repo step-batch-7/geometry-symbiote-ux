e52209d29cc2da7a1fca02d8c0e79469f2539c46
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2019-12-14 12:20:10 +0530

    Added visit method to class Point
    It visit the given reference and perform the given operation on point

--- src/point.js
+++ src/point.js
@@ -6,6 +6,9 @@ class Point {
   toString() {
     return `[Point @(${this.x},${this.y})]`;
   }
+  visit(givenFunction) {
+    return givenFunction(this.x, this.y);
+  }
 }
 
 module.exports = { Point };
--- test/testPoint.js
+++ test/testPoint.js
@@ -9,4 +9,21 @@ describe("Point", function() {
       assert.strictEqual(actual, `[Point @(2,3)]`);
     });
   });
+  describe("visit", function() {
+    it("should visit the given reference and perform the add operation on coordinates of point", function() {
+      const point = new Point(2, 3);
+      const actual = point.visit((x, y) => x + y);
+      assert.strictEqual(actual, 5);
+    });
+    it("should visit the given reference and perform the multiply operation on coordinates of point", function() {
+      const point = new Point(2, 3);
+      const actual = point.visit((x, y) => x * y);
+      assert.strictEqual(actual, 6);
+    });
+    it("should visit the given reference and perform the division operation on coordinates of point", function() {
+      const point = new Point(2, 3);
+      const actual = point.visit((x, y) => x / y);
+      assert.approximately(actual, 0.6, 0.5);
+    });
+  });
 });

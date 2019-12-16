52ec21ce2262b10c939e112b5d34a353fb871b0a
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2019-12-16 10:03:42 +0530

    Refactored findX and findY

--- src/line.js
+++ src/line.js
@@ -1,10 +1,18 @@
 const { Point } = require("./point.js");
+
 const arePointsEqual = function(point1, point2) {
   return point1.x == point2.x && point1.y == point2.y;
 };
 
 const areColinear = function(point1,point2,point3){
   return point1.x*(point2.y -point3.y)+point2.x*(point3.y -point2.y)+point3.x*(point1.y - point2.y)===0;
+}
+
+const arePointsInRange = function(midPoint,point1,point2){
+  if(point1>point2){
+    return point2 <=midPoint &&midPoint<=point;
+  }
+  return point2>=midPoint &&midPoint>=point1;
 }
 
 class Line {
@@ -38,13 +46,13 @@ class Line {
     if (!(line2 instanceof Line)) return false;
     return ((this.slope == line2.slope) && (!areColinear(this.endA,this.endB,line2.endA)));
   }
-  findX(y) {
-    if (!(y >= this.endA.y && y <= this.endB.y)) return NaN;
-    return (y - this.endA.y) / this.slope + this.endA.x;
+  findX(pointY) {
+    if (!arePointsInRange(pointY,this.endA.y,this.endB.y)) return NaN;
+    return (pointY - this.endA.y) / this.slope + this.endA.x;
   }
-  findY(x) {
-    if (!(x >= this.endA.x && x <= this.endB.x)) return NaN;
-    return (x - this.endA.x) * this.slope + this.endA.y;
+  findY(pointX) {
+    if (!arePointsInRange(pointX,this.endA.y,this.endB.y)) return NaN;
+    return (pointX - this.endA.x) * this.slope + this.endA.y;
   }
   split() {
     const xMid = (this.endA.x + this.endB.x) / 2;

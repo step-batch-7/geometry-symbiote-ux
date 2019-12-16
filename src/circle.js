cf46a33ea80d9606214b0e362daac290c352e175
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2019-12-16 14:03:32 +0530

    Added isEqual method to class Circle
    It gives true if circle1 and circle2 are same

--- src/circle.js
+++ src/circle.js
@@ -1,10 +1,16 @@
+const { Point } = require("./point");
+
 class Circle {
   constructor(center, radius) {
-    this.center = { x: center.x, y: center.y };
+    this.center = new Point(center.x, center.y);
     this.radius = radius;
   }
   toString() {
     return `[Circle @(${this.center.x},${this.center.y}) radius ${this.radius}]`;
+  }
+  isEqual(other) {
+    if (!(other instanceof Circle)) return false;
+    return this.radius === other.radius && this.center.isEqualTo(other.center);
   }
 }
 
--- test/testCircle.js
+++ test/testCircle.js
@@ -3,10 +3,18 @@ const { Circle } = require("../src/circle");
 
 describe("Circle", function() {
   describe("toString", function() {
-    it.only("Gives the string representation of the circle", function() {
+    it("Gives the string representation of the circle", function() {
       const center = { x: 1, y: 2 };
       const circle = new Circle(center, 5);
       assert.strictEqual(circle.toString(), `[Circle @(1,2) radius 5]`);
+    });
+  });
+  describe("isEqual", function() {
+    it.only("gives true if the both circles are at same location and are of same size and instance", function() {
+      const center = { x: 2, y: 2 };
+      const circle1 = new Circle(center, 5);
+      const circle2 = new Circle(center, 5);
+      assert.ok(circle1.isEqual(circle2));
     });
   });
 });

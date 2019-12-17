cacf0de0fe95d086d7a3ff36b99fbbfbf2da9bf8
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2019-12-17 13:50:15 +0530

    Refactored covers method in class Circle

--- src/circle.js
+++ src/circle.js
@@ -34,7 +34,7 @@ class Circle {
 
   covers(point) {
     if (!(point instanceof Point)) return false;
-    return this.radius <= this.center.findDistanceTo(point);
+    return this.center.findDistanceTo(point) <= this.radius;
   }
 }
 
